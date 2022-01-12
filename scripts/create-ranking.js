5af8f77626c760239f7ce85bf998be24efd998ac
okdyy75 <okdyy75@gmail.com>
2022-01-12 17:57:43 +0900

    ランキングjsonに作成日付追加（CIで差分が出ずにcommitエラーになるため）

--- scripts/create-ranking.js
+++ scripts/create-ranking.js
@@ -7,8 +7,15 @@ try {
     process.env.GOOGLE_APPLICATION_CREDENTIALS = `.gcp/google-analytics_credentials.json`
 
     // Imports the Google Analytics Data API client library.
+    const dayjs = require('dayjs')
+    const utc = require('dayjs/plugin/utc.js')
+    const timezone = require('dayjs/plugin/timezone.js')
     const fs = require('fs')
     const { BetaAnalyticsDataClient } = require('@google-analytics/data')
+
+    dayjs.extend(timezone)
+    dayjs.extend(utc)
+    dayjs.tz.setDefault('Asia/Tokyo')
 
     // Using a default constructor instructs the client to use the credentials
     // specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
@@ -43,7 +50,17 @@ try {
                 pv: row.metricValues[0].value,
             })
         })
-        fs.writeFileSync('data/ranking.json', JSON.stringify(rankings, null, 4))
+        fs.writeFileSync(
+            'data/ranking.json',
+            JSON.stringify(
+                {
+                    items: rankings,
+                    createdAt: dayjs().toISOString(),
+                },
+                null,
+                4
+            )
+        )
     }
 
     runReport()
